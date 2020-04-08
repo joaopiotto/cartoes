20d3bddf049147e5adcd887887b959cfa685a0df
João Gilberto de Souza Piotto <joao.piotto@kroton.com.br>
2020-04-08 17:26:35 -0300

    Adicionando Swagger

--- src/main/java/com/cartoes/api/security/config/WebSecurityConfig.java
+++ src/main/java/com/cartoes/api/security/config/WebSecurityConfig.java
@@ -58,7 +58,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.authorizeRequests()
-				.antMatchers("/auth/**").permitAll()
+				.antMatchers(
+						"/auth/**",
+						"/swagger-ui.html",
+						"/swagger-resources/**",
+						"/configuration/security",
+						"/webjars/**",
+						"/v2/api-docs"
+						).permitAll()
 				.anyRequest().authenticated();
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(),
 				UsernamePasswordAuthenticationFilter.class);
