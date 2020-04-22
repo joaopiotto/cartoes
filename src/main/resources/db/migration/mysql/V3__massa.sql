719cf43d6369bfb2b136d571ee991a7013bd4b01
João Gilberto de Souza Piotto <joao.piotto@kroton.com.br>
2020-04-22 12:13:57 -0300

    Excluindo arquivos de dados

--- src/main/resources/db/migration/mysql/V3__massa.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `regra` (`id`, `nome`, `descricao`, `ativo`) VALUES
-(NULL, 'ROLE_ADM_USUARIO', 'Permite acesso aos serviços de usuário', true);
-
-INSERT INTO `regra` (`id`, `nome`, `descricao`, `ativo`) VALUES
-(NULL, 'ROLE_EXCLUSAO', 'Permite exclusão de cartões', true);
